b94ff87eebb705beac69f9b49ee202261ecbcc94
Masayuki1009 <smasayukii1009@gmail.com>
2022-07-16 13:53:32 +0900

    setup server

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-print("hello Im Masayuki")
+from fastapi import FastAPI, APIRouter
+
+
+app = FastAPI(id="first fastAPI server", openapi_url="/openapi.json")
+api_router = APIRouter()
+
+@api_router.get(path="/")
+def hello():
+          print("hello")
+          return "hello"
+
+app.include_router(api_router)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app=app, host="0.0.0.0", port=5555, log_level="debug")
