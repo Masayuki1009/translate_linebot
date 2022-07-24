76897f36e7e4def663f66b5f1bc684e7db208436
Masayuki1009 <smasayukii1009@gmail.com>
2022-07-24 15:54:56 +0900

    revised run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-python app/main.py
+export APP_MODULE=${APP_MODULE-app.main:app}
+export HOST=${HOST:-0.0.0.0}
+export PORT=${PORT:-5555}
+
+exec uvicorn --reload --host $HOST --port $PORT "$APP_MODULE"
